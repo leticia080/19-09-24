cd163ed5e5047e242d566f4600c187beabb6fabd
leticia080 <126690016+leticia080@users.noreply.github.com>
2024-10-01 07:48:38 -0300

    01-10-24

--- 12-09-24-main/12-09-24-main/29-08-24-main (2)/29-08-24-main/script.js
+++ 12-09-24-main/12-09-24-main/29-08-24-main (2)/29-08-24-main/script.js
@@ -54,7 +54,11 @@ mostraAlternativas();
 function mostraAlternativas() {
 for (const alternativa of perguntaAtual.alternativas) {
 const botaoAlternativas = document.createElement("button");
-botaoAlternativas.textContent = alternativa;
+botaoAlternativas.textContent = alternativa.texto;
+botaoAlternativas.addEventListener("click",function(){
+    atual++;
+    mostraPergunta();
+})
 caixaAlternativas.appendChild(botaoAlternativas);
 }
 }
